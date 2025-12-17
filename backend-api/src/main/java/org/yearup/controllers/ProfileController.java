2da889b252d3459eecc9a84b1e2d8d1d2a6f75af
Billngoga <ngogaabill@example.com>
2025-12-17 11:47:13 -0600

    POST & PUT working for Profile.

---
+++ backend-api/src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,39 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("profile")
+@CrossOrigin
+public class ProfileController {
+    private ProfileDao profileDao;
+    private UserDao userDao;
+
+    @Autowired
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping
+    public Profile getProfileDao(Principal principal) {
+        String name = principal.getName();
+        User user = userDao.getByUserName(name);
+        Profile profile = profileDao.getByUserId(user.getId());
+        return profile;
+    }
+
+    @PutMapping
+    public Profile updateProfile(Principal principle, Profile profile) {
+        return null;
+    }
+}
+
+
--- backend-api/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ backend-api/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -8,21 +8,17 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 @Component
-public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
-{
-    public MySqlProfileDao(DataSource dataSource)
-    {
+public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao {
+    public MySqlProfileDao(DataSource dataSource) {
         super(dataSource);
     }
 
     @Override
-    public Profile create(Profile profile)
-    {
+    public Profile create(Profile profile) {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
                 " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try(Connection connection = getConnection())
-        {
+        try (Connection connection = getConnection()) {
             PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
             ps.setInt(1, profile.getUserId());
             ps.setString(2, profile.getFirstName());
@@ -37,11 +33,43 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
             ps.executeUpdate();
 
             return profile;
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Override
+    public Profile getByUserId(int userId) {
+        String sql = "SELECT * FROM profiles WHERE user_id = ?";
+
+        try (Connection connection = getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, userId);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Profile profile = new Profile();
+                profile.setUserId(rs.getInt("user_id"));
+                profile.setFirstName(rs.getString("first_name"));
+                profile.setLastName(rs.getString("last_name"));
+                profile.setPhone(rs.getString("phone"));
+                profile.setEmail(rs.getString("email"));
+                profile.setAddress(rs.getString("address"));
+                profile.setCity(rs.getString("city"));
+                profile.setState(rs.getString("state"));
+                profile.setZip(rs.getString("zip"));
+                return profile;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Profile update(Profile profile) {
+        return null;
+    }
 }
